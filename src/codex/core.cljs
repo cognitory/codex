470de0332f4e5385ab14d273051ea8755ff02bfc
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-03-02 17:15:37 -0500

    used "id" instead of "key"

--- src/codex/core.cljs
+++ src/codex/core.cljs
@@ -18,10 +18,10 @@
                 (doseq [file files]
                   (GET (str SITE_URL (file :path))
                     {:handler (fn [content]
-                                (let [key (string/replace-first (file :name) #"\.md" "")]
-                                  (swap! app-state assoc-in [:guides key] {:key key
-                                                                           :path (file :path)
-                                                                           :content content})))})))})
+                                (let [id (string/replace-first (file :name) #"\.md" "")]
+                                  (swap! app-state assoc-in [:guides id] {:id id
+                                                                          :path (file :path)
+                                                                          :content content})))})))})
 
   (GET (str REPO_URL "contents/tldrs")
     {:response-format :json
@@ -30,10 +30,10 @@
                 (doseq [file files]
                   (GET (str SITE_URL (file :path))
                     {:handler (fn [content]
-                                (let [key (string/replace-first (file :name) #"\.md" "")]
-                                  (swap! app-state assoc-in [:tldrs key] {:key key
-                                                                          :path (file :path)
-                                                                          :content content})))})))}))
+                                (let [id (string/replace-first (file :name) #"\.md" "")]
+                                  (swap! app-state assoc-in [:tldrs id] {:id id
+                                                                         :path (file :path)
+                                                                         :content content})))})))}))
 
 (defn app-view []
   [:div
@@ -41,13 +41,13 @@
    [:h2 "Guides"]
    [:div.guides
     (for [[_ guide] (@app-state :guides)]
-      [:a {:key (guide :key)
-           :href (guide :path)} (guide :key)]) ]
+      [:a {:key (guide :id)
+           :href (guide :path)} (guide :id)]) ]
    [:h2 "TLDRs"]
    [:div.tldrs
     (for [[_ tldr] (@app-state :tldrs)]
-      [:a {:key (tldr :key)
-           :href (tldr :path)} (tldr :key)])]])
+      [:a {:key (tldr :id)
+           :href (tldr :path)} (tldr :id)])]])
 
 (defn init []
   (r/render-component [app-view] (.-body js/document)))
