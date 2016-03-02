7467622781755a2e668743bff5fabf83cd0a22e7
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-03-02 16:32:43 -0500

    get list of guides and tldrs from github

--- src/codex/core.cljs
+++ src/codex/core.cljs
@@ -1,5 +1,35 @@
-(ns codex.core)
+(ns codex.core
+  (:require [ajax.core :refer [GET POST]]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
-(println "hello world")
+(defonce app-state (atom {}))
+
+(defonce REPO_URL "https://api.github.com/repos/cognitory/codex/")
+(defonce SITE_URL "http://cognitory.github.io/codex/")
+
+(defn key-by [k arr]
+  (reduce (fn [memo i]
+            (assoc memo (k i) i)) arr {}))
+
+(defn init []
+  (GET (str REPO_URL "contents/guides")
+    {:response-format :json
+     :keywords? true
+     :handler (fn [files]
+                (swap! app-state assoc :guides
+                       (->> files
+                            (map (fn [f]
+                                   {:key (string/replace-first (f :name) #"\.md" "")
+                                    :file (f :name)}))
+                            (key-by :key))))})
+  (GET (str REPO_URL "contents/tldrs")
+    {:response-format :json
+     :keywords? true
+     :handler (fn [files]
+                (swap! app-state assoc :tldrs
+                       (->> files
+                            (map (fn [f]
+                                   {:key (string/replace-first (f :name) #"\.md" "")
+                                    :file (f :name)})))))}))
