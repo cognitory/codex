32b0536a9b119d65599e1d89a1a7cfc7f8659f35
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-03-14 16:41:51 -0400

    add cljsjs/highlight

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [cljsjs/js-yaml "3.3.1-0"]
                  [markdown-clj "0.9.86"]
                  [garden "1.3.2"]
-                 [fipp "0.6.4"]]
+                 [fipp "0.6.4"]
+                 [cljsjs/highlight "8.4-0"]]
 
   :plugins [[lein-cljsbuild "1.1.2"]
             [lein-figwheel "0.5.0-6"]]
