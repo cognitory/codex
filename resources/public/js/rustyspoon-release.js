07e1e3b76bf97bfa4b574dbd0a55f82e71d9f05a
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-03-14 16:11:31 -0400

    build release js

---
+++ resources/public/js/rustyspoon-release.js
@@ -0,0 +1,4 @@
+var CLOSURE_UNCOMPILED_DEFINES = null;
+if(typeof goog == "undefined") document.write('<script src="/codex/resources/public/js/rustyspoon-release/goog/base.js"></script>');
+document.write('<script src="/codex/resources/public/js/rustyspoon-release/cljs_deps.js"></script>');
+document.write('<script>if (typeof goog != "undefined") { goog.require("rustyspoon.orbit"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
